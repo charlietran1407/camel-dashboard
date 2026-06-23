af07b8fe8094e80762e2d9a571b247955c49d0da
ChauTK <charlie.tran1407@gmail.com>
2026-06-23 13:07:23 +0700

    chore(examples): remove local examples directory All examples have been migrated to the dedicated camel-dashboard-examples repository.

--- examples/rss-to-db/1.init_database.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE news_world (
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    link TEXT NOT NULL,
-    pub_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Set the default value to the current time
-    description TEXT, 
-    source TEXT
-);
